22b54826cc89239f9d1dc86d0dbff07930cb2744
Fyke Lleva <floterina@gmail.com>
2024-05-02 21:41:49 +0800

    updated

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="floating-labels.css" rel="stylesheet">
+    <link href="static/css/floating-labels.css" rel="stylesheet">
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -23,7 +23,7 @@
   <body>
     <form class="form-signin" action="ehr_list.php">
       <div class="text-center mb-4">
-        <img class="mb-4" src="logo.png" alt="" width="100%">
+        <img class="mb-4" src="static/images/logo.png" alt="" width="100%">
         <!--<h1 class="h3 mb-3 font-weight-normal">Login</h1>-->
       </div>
 
@@ -49,3 +49,5 @@
     </form>
   </body>
 </html>
+
+<script src="javascript/login.js"></script>
